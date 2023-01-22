42184371e35aec2095489bf5e9dc97bb10556042
Ahmad Chaachouh <ahmad.chaachouh.cad@gmail.com>
2023-01-22 14:41:12 +0200

    added UI

--- rock.js
+++ rock.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let playerScore = 0;
+
 function getComputerChoice(){
     let x = Math.floor(Math.random()*3);
     return x;
@@ -9,6 +12,11 @@ function playerSelection(){
 }
 
 function play(comp,player){
+
+     if (playerScore == 5 || computerScore == 5) {
+        fin.textContent = `final score : player : ${playerScore}, computer : ${computerScore}.`
+        return;
+    }
     
     let select = player;
     let y = 0;
@@ -23,36 +31,39 @@ function play(comp,player){
 
 
     if (y == comp){
-        console.log("It's a draw");
-        return 3;
+        result.textContent = "draw";
     }
     else if (y > comp && Math.abs(y-comp) < 2){
-        console.log("palyer wins this one");
-        return 2;
-    }
+        result.textContent = "win";
+        playerScore++; 
+        playerscore.textContent = playerScore; 
+     }
     else if (comp > y && Math.abs(comp-y) < 2){
-        console.log("comp wins this one");
-        return -2;
+        result.textContent = "lose";
+        computerScore++;
+        compscore.textContent = computerScore;
     }
 }
-function game(){
-    let result = 0, p = 0, c = 0;
-    for(i = 0; i < 5; i++){
-        comp = getComputerChoice();
-        player = playerSelection();
-        result = play(comp, player);
-        if(result == 2){
-            p++;
-        }else if(result == -2){
-            c++;
-        }
-        console.log(`palyer : ${p}, comp : ${c}`);  
-    }
-    if(p > c){
-        console.log("CONgrats! Yoiu win!");
-    }else if (p == c){
-        console.log("It's a draw...");
-    }else{
-        console.log("OOps! Computer wins");
-    }
-}
+// function game(){
+//     let result = 0, p = 0, c = 0;
+//     for(i = 0; i < 5; i++){
+//         comp = getComputerChoice();
+//         player = playerSelection();
+//         result = play(comp, player);
+//         if(result == 2){
+//             p++;
+//         }else if(result == -2){
+//             c++;
+//         }
+//         console.log(`palyer : ${p}, comp : ${c}`);  
+//     }
+//     if(p > c){
+//         console.log("CONgrats! Yoiu win!");
+//     }else if (p == c){
+//         console.log("It's a draw...");
+//     }else{
+//         console.log("OOps! Computer wins");
+//     }
+// }
+
+
